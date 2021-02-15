1c14a956454affce537d78ab5960cba45f6a9210
caseyahoffman <caseyhoff97@gmail.com>
2021-02-15 09:58:10 -0500

    produced some figs

--- ulta-dataviz.R
+++ ulta-dataviz.R
@@ -1,27 +1,50 @@
 library(tidyverse)
 
 ulta <- read.csv('ulta-cleaned.csv')
-
 ulta = ulta %>%
   mutate(X = NULL, brand = as.factor(brand)) # X: 'row index' column
 
-ggplot(data = ulta) + geom_histogram(aes(x = avg_review), binwidth=.1) 
-# add line with median 
+# price histogram
+ggplot(data = ulta) + geom_histogram(aes(x = price_usd), binwidth = 5, fill='blue', color='black') +
+  geom_vline(aes(xintercept = median(price_usd)), colour='red') +
+  ggtitle('Histogram of Moisturizer by Price') +
+  ggsave("price_hist.png")
 
-
-top_prods = ulta %>%
-  filter(avg_review > mean(avg_review), n_review > mean(n_review))
-
-top_prods %>%
-  arrange(avg_review, ascending=FALSE)
+top_prods = ulta %>% 
+  filter(n_review > 100) %>%
+  mutate('price_range' = ifelse(price_usd < 25, "Under $25", 
+                                ifelse(price_usd < 50, "$25-50", "$50 or more" ))) %>%
+  mutate(price_range = as.factor(price_range))
 
 
 summary(top_prods)
 
-by_brand = ulta %>%
-  group_by(brand) %>% 
-  summarise(num_prods = n(), avg_rating = mean(avg_review)) %>%
-  filter(num_prods > 2)
+review_plot = ggplot(data = top_prods) + 
+  geom_boxplot(aes(x = price_range, y = avg_review, fill = price_range))
 
-ggplot(data = by_brand) + geom_bar(aes(x = brand, y = avg_rating), stat='identity')
+review_plot +
+  scale_x_discrete(limits = c("Under $25", "$25-50", "$50 or more")) +
+  ggtitle("Boxplot of Average Review, by Price Range") +
+  ggsave("review-boxplot.png")
 
+ggplot(data = top_prods) + 
+  geom_density(aes(x = avg_review, color = price_range)) +
+  ggtitle("Density of Average Product Review, by Price Range") + 
+  ggsave("review-density.png")
+
+top_prods = top_prods %>% 
+  mutate('bestseller_status' = avg_review*n_review)
+
+
+top_prods %>% slice_max(bestseller_status, n=10) %>% 
+  ggplot() + geom_col(aes(x = product, y = n_review, fill = brand)) +
+  ggtitle("Number of Reviews of Bestsellers") +
+  theme(axis.text.x = element_blank()) +
+  ggsave("bar-bestsellerreviews.png")
+
+
+top_prods %>% slice_max(bestseller_status, n=10) %>% 
+  ggplot() + geom_col(aes(x = product, y = price_usd, fill = brand)) +
+  ggtitle("Cost of Bestsellers") + 
+  theme(axis.text.x = element_blank()) +
+  ggsave("bar-bestseller-cost.png")
